191f39e97901a4d2c66d2be86a53e330c25ed374
Solly Ross <sross@redhat.com>
2018-09-06 21:05:08 -0400

    Filled in types
    The types for CoffeeMaker and Cup are now in place.

--- pkg/apis/coffee/v1alpha1/coffeemaker_types.go
+++ pkg/apis/coffee/v1alpha1/coffeemaker_types.go
@@ -25,15 +25,56 @@ import (
 
 // CoffeeMakerSpec defines the desired state of CoffeeMaker
 type CoffeeMakerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// The supported cup sizes for this coffee maker.
+	CupSizes []int `json:"cupSizes"`
+	// The general descriptors for this coffemaker (like labels, could
+	// be extended to support quantities later)
+	// +optional
+	Descriptors map[string]string `json:"descriptors,omitempty"`
 }
 
 // CoffeeMakerStatus defines the observed state of CoffeeMaker
 type CoffeeMakerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// extra status information about the coffeemaker
+	// +optional
+	Conditions []CoffeeMakerCondition `json:"conditions,omitempty"`
 }
+
+// CoffeemakerCondition defines a status condition for a CoffeeMaker.
+type CoffeeMakerCondition struct {
+	// Type is the type of the condition.
+	Type CoffeeMakerConditionType `json:"type"`
+	// Status is the status of the condition.
+	// Can be True, False, Unknown.
+	Status ConditionStatus `json:"status"`
+	// Last time we probed the condition.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+	// Unique, one-word, CamelCase reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// Human-readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+type (
+	ConditionStatus          string
+	CoffeeMakerConditionType string
+)
+
+// These are valid condition statuses. "ConditionTrue" means a resource is in the condition.
+// "ConditionFalse" means a resource is not in the condition. "ConditionUnknown" means kubernetes
+// can't decide if a resource is in the condition or not. In the future, we could add other
+// intermediate conditions, e.g. ConditionDegraded.
+const (
+	ConditionTrue    ConditionStatus = "True"
+	ConditionFalse   ConditionStatus = "False"
+	ConditionUnknown ConditionStatus = "Unknown"
+)
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- pkg/apis/coffee/v1alpha1/cup_types.go
+++ pkg/apis/coffee/v1alpha1/cup_types.go
@@ -25,15 +25,102 @@ import (
 
 // CupSpec defines the desired state of Cup
 type CupSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// the type of drink (coffee)
+	Drink DrinkType `json:"drink"`
+	// the size of the drink
+	// +optional
+	Size DrinkSize `json:"size,omitempty"`
+	// a description of the desired drink
+	// +optional
+	PodSpec PodSpec `json:"podSpec,omitempty"`
+}
+
+// DrinkSize defines the requests and limits for drink cup sizes
+type DrinkSize struct {
+	// the requested size for the cup
+	Request int `json:"request"`
+	// the maximum size tolerated for the cup
+	// +optional
+	MaxLimit int `json:"maxLimit,omitempty"`
+	// the minimum size tolerated for the cup
+	// +optional
+	MinLimit int `json:"minLimit,omitempty"`
+}
+
+// PodSpec defines the kind of Keurig pod desired for brewing the drink
+// (corresponds to descriptors on a CoffeeMaker)
+type PodSpec struct {
+	// the requested (want) features of the drink
+	// +optional
+	Requests map[string]string `json:"requests,omitempty"`
+	// the required (must have) features of the drink
+	// +optional
+	Requirements map[string]string `json:"requirements,omitempty"`
 }
 
 // CupStatus defines the observed state of Cup
 type CupStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// the brew information for the drink
+	// +optional
+	Brew BrewStatus `json:"brew,omitempty"`
+	// the actual size of the drink
+	// +optional
+	Size int `json:"size,omitempty"`
+	// the actual descriptors for the drink
+	// +optional
+	Descriptors map[string]string `json:"descriptors,omitempty"`
+	// extra status information about the drink
+	// +optional
+	Conditions []CupCondition `json:"conditions,omitempty"`
 }
+
+// BrewStatus represents if and when the drink was brewed
+type BrewStatus struct {
+	// the state of the brewing process
+	State BrewState `json:"state"`
+	// when the brewing process started
+	// +optional
+	Start metav1.Time `json:"start,omitempty"`
+	// when the brewing process ended
+	// +optional
+	End metav1.Time `json:"end,omitempty"`
+}
+
+// CoffeemakerCondition defines a status condition for a CoffeeMaker.
+type CupCondition struct {
+	// Type is the type of the condition.
+	Type CupConditionType `json:"type"`
+	// Status is the status of the condition.
+	// Can be True, False, Unknown.
+	Status ConditionStatus `json:"status"`
+	// Last time we probed the condition.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+	// Unique, one-word, CamelCase reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// Human-readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+type (
+	CupConditionType string
+	BrewState        string
+	DrinkType        string
+)
+
+const (
+	CoffeeDrink DrinkType = "Coffee"
+	TeaDrink    DrinkType = "Tea" // HTTP 418
+
+	NotBrewed BrewState = "NotBrewed"
+	Brewed    BrewState = "Brewed"
+	Brewing   BrewState = "Brewing"
+)
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
